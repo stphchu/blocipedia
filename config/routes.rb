0af6e3b0381af944b32944be5e8f4c1d982bfc84
Stephanie <esceeaitchyu@gmail.com>
2018-08-10 22:53:52 -0700

    Complete User Story 2: User Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
 
-  root to: 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 
 end
